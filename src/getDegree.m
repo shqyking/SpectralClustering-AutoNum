5d704052e4bca73dc87805937263814bb799e425
shqyking <sunqyoung@gmail.com>
2015-05-04 16:48:39 -0400

    seperate files

--- src/getDegree.m
+++
@@ -1,9 +0,0 @@
-function D = getDegree(W)
-% Return the degree matrix D of weight matrix W
-% D is a N x N diagonal matrix
-N = size(W,1);
-D = zeros(N);
-for i=1:N
-   D(i,i) = sum(W(i,:)); 
-end
-end
